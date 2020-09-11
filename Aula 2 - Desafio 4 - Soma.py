3ee39aa752b33bc3e46218087c819565321380e6
Lucas Lima <lucas.mateus.rl7@gmail.com>
2020-09-11 11:51:08 -0300

    Aula 2 - Fazer desafios de soma e verificação se os numeros sao positivos negativos e neutro

---
+++ Aula 2 - Desafio 4 - Soma.py
@@ -0,0 +1,4 @@
+def retornar_soma(a,b,c):
+    return a + b + c
+
+print(retornar_soma(1,5,2))
---
+++ Aula 2 - Desafio 5 - Positivo e Negativo.py
@@ -0,0 +1,9 @@
+def retornar_verificacao_numero(x):
+    if x > 0:
+        print('POSITIVO')
+    elif x < 0:
+        print('NEGATIVO')
+    else:
+        print('NEUTRO')
+
+print(retornar_verificacao_numero(-10))
