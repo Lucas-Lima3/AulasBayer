f822985db473ef626e0f41b86e2c2659fea366c1
Lucas Lima <lucas.mateus.rl7@gmail.com>
2020-09-17 21:53:54 -0300

    Aula 2 - Fazer Exercício 2 para casa

--- Aula 2 - Exercícios para casa.py
+++ Aula 2 - Exercícios para casa.py
@@ -15,3 +15,26 @@ for item in dadospessoais:
     print(f'{item} : {dadospessoais[item]}')
 
 print()
+
+
+
+"""
+Retornar maior valor
+    Crie uma função que receba como argumento 3 valores, e retorne qual o maior valor.
+"""
+
+print('EXERCÍCIO 2')
+
+numeros = []
+
+numero1 = float(input('Digite um número: '))
+numero2 = float(input('Digite outro número: '))
+numero3 = float(input('Digite outro número: '))
+
+numeros.append(numero1)
+numeros.append(numero2)
+numeros.append(numero3)
+
+print(max(int(numero) for numero in numeros))
+
+print()
